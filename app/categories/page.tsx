0e0b770ef0201b8fe44affa68bb5c5294a969bbe
sajib <sahasojib0155@gmail.com>
2025-09-02 07:59:05 +0600

    dsfsdf

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -2,11 +2,16 @@ import { HeadingTitle } from "@/components/heading-title";
 import { CategoryContent } from "./_components/category-content";
 import { getCategories } from "@/actions/category";
 
-export default async function CategoryPage({ searchParams }: { searchParams?: { page?: string } }) {
-  
-  const currentPage = Number(searchParams?.page || "1");
+interface CategoryPageProps {
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
 
+export default async function CategoryPage({ searchParams }: CategoryPageProps) {
+  const page = Array.isArray(searchParams?.page)
+    ? searchParams?.page[0]
+    : searchParams?.page;
 
+  const currentPage = Number(page || "1");
 
   return (
     <div className="min-h-screen py-10">
