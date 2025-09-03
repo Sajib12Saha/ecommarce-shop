3d0ef127fdbc9f434a1ceac44a93fbf472a2c94b
sajib <sahasojib0155@gmail.com>
2025-09-03 10:15:45 +0600

    fds

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ProductsPage = async({searchParams}:Props) => {
   const resolvedSearchParams = await searchParams
   return (
-    <div className="flex gap-x-2 w-full px-2">
+    <div className="flex gap-x-2 w-full px-1">
       <ProductState productName={resolvedSearchParams.productName || ""}/>
     </div>
   );
